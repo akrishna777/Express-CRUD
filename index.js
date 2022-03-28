918dae01d2a7f3f5f092e82d1e56e86006ec8d79
Aravind Krishna <akaravindk59@gmail.com>
2022-03-29 04:08:33 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 3000
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(cors())
+
+let details = [
+  {
+    id: 1,
+    name: 'Praveen',
+    email: 'praveen@gmail.com',
+  },
+  {
+    id: 2,
+    name: 'Akash',
+    email: 'akash@gmail.com',
+  },
+  {
+    id: 3,
+    name: 'Manju',
+    email: 'manju@gmail.com',
+  },
+]
+
+app.get('/:id', (req, res) => {
+  let id = req.params.id
+  const data = details.filter((item) => item.id == id)
+  res.send(data)
+})
+
+app.get('/', function (req, res) {
+  res.send(details)
+})
+
+app.post('/', async (req, res) => {
+  details.push(req.body)
+  res.send({ message: 'OK' })
+})
+
+app.put('/', async (req, res) => {
+  for (let i = 0; i < details.length; i++) {
+    if (req.body.id == details[i].id) {
+      details[i].name = req.body.name
+      details[i].email = req.body.email
+    }
+  }
+  res.send({ message: 'Updated' })
+})
+
+app.delete('/:id', async (req, res) => {
+  let index
+  for (let i = 0; i < details.length; i++) {
+    if (req.params.id == details[i].id) {
+      index = i
+    }
+  }
+  details.splice(index, 1)
+  res.send({ message: 'Deleted' })
+})
+
+app.listen(PORT, () => {
+  console.log('App is running on port:', PORT)
+})
